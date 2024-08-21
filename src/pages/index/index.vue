4f1b8cbe9d4feca5828806ae7c8a8e52edca0949
aiShuiJiaoDeXioShou <2832294398@qq.com>
2024-08-21 16:11:06 +0800

    first commit

---
+++ src/pages/index/index.vue
@@ -0,0 +1,45 @@
+<route lang="json5" type="home">
+{
+  style: {
+    navigationStyle: 'custom',
+    navigationBarTitleText: '首页',
+  },
+}
+</route>
+
+<template>
+  <view class="home-root">
+    <view class="container">
+      <!-- 左侧圆形部分 -->
+      <view class="circle">宇森科技</view>
+
+      <!-- 右侧矩形部分 -->
+      <view class="rectangle">
+        <text>成立历史，介绍来历，参与事情。</text>
+      </view>
+    </view>
+  </view>
+</template>
+
+<script lang="js" setup>
+// TODO
+</script>
+
+<style lang="scss" scoped>
+.home-root {
+  @apply bg-white min-h-screen flex items-center justify-center;
+}
+
+.container {
+  @apply flex items-center p-4;
+}
+
+.circle {
+  @apply w-20 h-20 rounded-full flex items-center justify-center text-center bg-blue-200 text-black;
+}
+
+.rectangle {
+  @apply w-60 h-20 ml-4 flex items-center justify-center text-black border rounded-lg;
+  border-color: #000000;
+}
+</style>
